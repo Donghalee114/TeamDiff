2f4184c14b8e606443af6c6d298f2a7343383225
donghalee <donghalee114@m2mtech.co.kr>
2025-05-22 13:50:20 +0900

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
-import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SearchPlayer from './serachplayer';
+import Terms from './page/terms';
+import Privacy from './page/privacy';
+
 
 function App() {
 
-
   return (
-<SearchPlayer/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchPlayer />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        {/* 여기에 기존 홈, 기타 라우트도 같이 넣으면 돼 */}
+      </Routes>
+    </Router>
+
+    
   );
 }
 
